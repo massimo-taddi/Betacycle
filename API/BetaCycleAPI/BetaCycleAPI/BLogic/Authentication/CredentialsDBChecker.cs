7cbdedbb7aad5a2df4274a57bc296ad8dcc3325c
Flarg47 <76622210+Flarg47@users.noreply.github.com>
2024-04-16 17:45:17 +0200

    Authentication v0.0.2

--- API/BetaCycleAPI/BetaCycleAPI/BLogic/Authentication/CredentialsDBChecker.cs
+++ API/BetaCycleAPI/BetaCycleAPI/BLogic/Authentication/CredentialsDBChecker.cs
@@ -2,42 +2,33 @@
 using Microsoft.EntityFrameworkCore;
 using EncryptData;
 using BetaCycleAPI.Models.Enums;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace BetaCycleAPI.BLogic.Authentication
 {
     public class CredentialsDBChecker
     {
-        private readonly AdventureWorksLt2019Context _context;
-        private readonly AdventureWorks2019CredentialsContext _credentials;
-
-        public CredentialsDBChecker(AdventureWorksLt2019Context context, AdventureWorks2019CredentialsContext credentials)
-        {
-            _context = context;
-            _credentials = credentials;
-        }
-
-        public CredentialsDBChecker CreateChecker()
-        {
-            return new CredentialsDBChecker(_context, _credentials);
-        }
+        SqlCommand sqlCmd = new();
+        SqlConnection myConn = new();
         public async Task<DBCheckResponse> ValidateLoginAsync(string user, string pwd)
         {
             DBCheckResponse response = DBCheckResponse.NotFound;
             try
             {
-                var credLoginNew = await _credentials.Credentials.Where(c => c.Email == user).ToListAsync();
-                var credLoginOld = await _context.Customers.Where(c => c.EmailAddress == user).ToListAsync();
-                if (credLoginNew.Any()) //new db
-                {
-                    if ((EncryptData.CypherData.DecryptSalt(pwd, credLoginNew.First().SaltHash)).Equals(credLoginNew.First().PasswordHash))
-                    {
-                        response = DBCheckResponse.FoundMigrated;
-                    }
-                }
-                else if (credLoginOld.Any()) //old db
-                {
-                    response = DBCheckResponse.FoundNotMigrated;
-                }
+                dynamic appSettings = JsonConvert.DeserializeObject<string>(File.ReadAllText(@"..\..\appsettings.json"));
+                myConn.ConnectionString = appSettings.ConnectionString;
+                //if (credLoginNew.Any()) //new db
+                //{
+                //    if ((EncryptData.CypherData.DecryptSalt(pwd, credLoginNew.First().SaltHash)).Equals(credLoginNew.First().PasswordHash))
+                //    {
+                //        response = DBCheckResponse.FoundMigrated;
+                //    }
+                //}
+                //else if (credLoginOld.Any()) //old db
+                //{
+                //    response = DBCheckResponse.FoundNotMigrated;
+                //}
             }catch(Exception ex)
             {
                 throw;
