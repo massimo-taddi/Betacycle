64ba5cfd02fce8919784d0fa28410f2092d70d76
Tommaso De Pascale <tomtom4830@gmail.com>
2024-05-02 11:40:10 +0200

    Update app.routes.ts

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './core/login/login.component';
 import { SignUpComponent } from './core/sign-up/sign-up.component';
 import { SearchComponent } from './features/search/search.component';
 import { PrivateAreaComponent } from './features/private-area/private-area.component';
+import { PersonalOrdersComponent } from './features/private-area/personal-orders/personal-orders.component';
 
 export const routes: Routes = [
   {
@@ -31,5 +32,11 @@ export const routes: Routes = [
   {
     path: 'personal',
     component: PrivateAreaComponent,
+    children: [
+      {
+        path: 'myorders',
+        component: PersonalOrdersComponent
+      }
+    ]
   },
 ];
