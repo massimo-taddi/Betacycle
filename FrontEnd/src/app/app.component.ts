0d8445680ac4d43686c5a369fe9bfa6ac1adc2ac
boh <128642376+FlaRG477@users.noreply.github.com>
2024-05-16 13:09:17 +0200

    Home + Footer(beta)

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { NgStyle } from '@angular/common';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, NgStyle],
+  imports: [RouterOutlet, NavbarComponent, NgStyle, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- FrontEnd/src/app/features/home/home.component.ts
+++ FrontEnd/src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TagModule } from 'primeng/tag';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { ProductService } from '../../shared/services/product.service';
+import { Product } from '../../shared/models/Product';
 
 @Component({
   selector: 'app-home',
@@ -21,10 +23,31 @@ import { NgModule } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  images: any[] = [
-    '../../../assets/images/BiciclettaProva1.png',
-    '../../../assets/images/BiciclettaProva2.jng',
-    '../../../assets/images/BiciclettaProva3.jng',
-  ];
+export class HomeComponent implements OnInit{
+  constructor(private http: ProductService){
+  }
+  responsiveOptions : any[]=[
+    {
+        breakpoint: '--bs-breakpoint-lg',
+        numVisible: 2,
+        numScroll: 3
+    }]
+    products: Product[] =[];
+    i: number =0;
+
+    ngOnInit(): void{
+      this.FillProducts()
+    }
+
+    FillProducts(){
+      //TEST DEL CAROUSEL CON FOR
+      for(this.i=708;this.i< 717;this.i++){
+        this.http.getProductById(this.i).subscribe({
+          next: (data: any) =>{
+            this.products.push(data);
+          }
+        });
+      }
+      console.log(this.products);
+    }
 }
