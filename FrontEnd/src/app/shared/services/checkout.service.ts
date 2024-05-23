d28c9d63798b204e2161f819e0a9ba19e2ed22fc
Tommaso De Pascale <tomtom4830@gmail.com>
2024-05-23 17:24:16 +0200

    Update checkout.service.ts

--- FrontEnd/src/app/shared/services/checkout.service.ts
+++ FrontEnd/src/app/shared/services/checkout.service.ts
@@ -1,16 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SalesOrderHeader } from '../models/SalesOrderHeader';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthenticationService) { }
 
   postSalesOrder(salesOrder: SalesOrderHeader): Observable<any> {
-    return this.http.post('https://localhost:7287/api/orders', salesOrder);
+    var header = new HttpHeaders();
+    this.authService.authJwtHeader$.subscribe((h) => (header = h));
+    return this.http.post('https://localhost:7287/api/orders', salesOrder, {headers: header});
   }
 }
